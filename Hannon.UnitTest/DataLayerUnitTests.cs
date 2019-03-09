9a0088a00c302762ae4480173cf6c307f4ba4b76
Patrick Hannon <patrick_hannon@hotmail.com>
2019-03-08 18:38:55 -0600

    unit test project for ecommere

---
+++ Hannon.UnitTest/DataLayerUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ECommerce.Data.Repository;
+using ECommerce.Data.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Diagnostics;
+
+namespace Hannon.UnitTest
+{
+    [TestClass]
+    class DataLayerUnitTests
+    {
+        public string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+
+        [TestMethod]
+        public void VerifyDataLayerRepositories()
+        {
+            var p = new ProductRepository(connectionString);
+            var product = p.GetById(7);
+            Assert.IsNotNull(product);
+
+            var c = new CategoryRepository(connectionString);
+            var count = c.Get().Count;
+            Assert.IsNotNull(c.Get());
+            Assert.IsTrue(count > 0, "c.GetCategories");
+        }
+    }
+}
--- Hannon.UnitTest/TwoFactorAuthUnitTests.cs
+++ Hannon.UnitTest/TwoFactorAuthUnitTests.cs
@@ -5,6 +5,10 @@ using hannon._2factorAuth;
 using hannon.TwoFactorAuth.Models;
 using hannon._2factorAuth.Models;
 using System.Web;
+using ECommerce.Controllers;
+using ECommerce.Data.Repository;
+using ECommerce.Models;
+using Moq;
 namespace Hannon.UnitTest
 {
     [TestClass]
@@ -19,15 +23,20 @@ namespace Hannon.UnitTest
         private string _accountSID;
         private int _twoFactorTimeOut;
         private ITwoFactorAuth _twoFactorAuth;
+        public string connectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+        private string _emailPassword;
+        private bool _twoFactorEnabled;
 
         public TwoFactorAuthUnitTests()
         {
             int.TryParse(ConfigurationManager.AppSettings["TwoFactorAuthTimeSpan"], out _twoFactorAuthTimeSpan);
             int.TryParse(ConfigurationManager.AppSettings["TwoFactorTimeOut"], out _twoFactorTimeOut);
+            bool.TryParse(ConfigurationManager.AppSettings["TwoFactorEnabled"], out _twoFactorEnabled);
             _twoFactorAuthCookie = ConfigurationManager.AppSettings["TwoFactorAuthCookie"];
             _twoFactorAuthSmtpHost = ConfigurationManager.AppSettings["TwoFactorAuthSmtpHost"];
             _twoFactorAuthFromEmail = ConfigurationManager.AppSettings["TwoFactorAuthFromEmail"];
             _twoFactorAuthFromPhone = ConfigurationManager.AppSettings["TwoFactorAuthFromPhone"];
+            _emailPassword = ConfigurationManager.AppSettings["EmailPassword"];
             _authToken = ConfigurationManager.AppSettings["TwilioAuthToken"];
             _accountSID = ConfigurationManager.AppSettings["TwilioAccountSID"];
 
@@ -39,14 +48,29 @@ namespace Hannon.UnitTest
                 TwoFactorAuthFromEmail = _twoFactorAuthFromEmail,
                 TwoFactorAuthFromPhone = _twoFactorAuthFromPhone,
                 AuthToken = _authToken,
-                AccountSID = _accountSID
+                AccountSID = _accountSID,
+                TwoFactorEnabled = _twoFactorEnabled,
+                EmailPassword = _emailPassword
             };
-
             //_identity = new TranparentIdentity();
             _twoFactorAuth = new TwoFactorAuth(smsConfigs);
         }
 
         [TestMethod]
+        public void VerifyDataLayerRepositories()
+        {
+            var p = new ProductRepository(connectionString);
+            var product = p.GetById(7);
+            Assert.IsNotNull(product);
+
+            var c = new CategoryRepository(connectionString);
+            var count = c.Get().Count;
+            Assert.IsNotNull(c.Get());
+            Assert.IsTrue(count > 0, "c.GetCategories");
+        }
+
+        [TestMethod]
+        [Ignore()]
         public void ValidateSMSIsSent()
         {
             try
@@ -68,6 +92,7 @@ namespace Hannon.UnitTest
         }
 
         [TestMethod]
+        [Ignore()]
         public void ValidateEmailIsSent()
         {
             try
@@ -89,12 +114,30 @@ namespace Hannon.UnitTest
             }
         }
 
-        public void TestLogin()
+        [TestMethod]
+        [Ignore()]
+        public void VerifyCodeTest()
         {
-            //AccountController acct = new AccountController();
-            //acct.SetFakeControllerContext();
-            //acct.SetBusinessObject(mockBO.Object);
+            AccountController acct = new AccountController();
+            //var mockedPrincipal = new Mock<WindowsPrincipal>(WindowsIdentity.GetCurrent());
 
+            //mockedPrincipal.SetupGet(x => x.Identity.IsAuthenticated).Returns(true);
+            //mockedPrincipal.SetupGet(x => x.Identity.Name).Returns("HANNON\\phannon");
+            //mockedPrincipal.Setup(x => x.IsInRole("Domain\\Group1")).Returns(true);
+            //mockedPrincipal.Setup(x => x.IsInRole("Domain\\Group2")).Returns(false);
+
+
+            var model = new VerifyCodeViewModel()
+            {
+                Code = "123456"
+            };
+
+            var mockHttpContext = MockHttpSession.FakeHttpContext();
+            var mockSession = mockHttpContext.Session;
+            mockSession["AuthCode"]= "123456";
+            var r = acct.VerifyCode(model);
+            Assert.IsNotNull(r);
+            
             //RedirectResult results = (RedirectResult)acct.LogOn(userName, password, rememberMe, returnUrl);
             //Assert.AreEqual(returnUrl, results.Url);
             //Assert.AreEqual(userName, acct.Session["txtUserName"]);
